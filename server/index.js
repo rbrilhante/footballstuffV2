9dfb3c122f15b31bea84da63664bfadb7bb6f0fe
Ricardo Brilhante <ricardo.brilhante@mercedes-benz.io>
2022-12-13 19:25:05 +0000

    works on my machine

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 var request = require('request');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 const app = express();
